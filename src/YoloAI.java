160a6ecf294c45f036ce24266d470fe54c3cd2f3
sammathews <sammathews@10.80.11.167>
2013-01-16 12:03:48 -0700

    Added main methods and classes to YoloAI

---
+++ src/YoloAI.java
@@ -0,0 +1,18 @@
+import core.LetterBag;
+import core.Player;
+import core.Word;
+
+public class YoloAI extends Player {
+
+	public YoloAI(LetterBag ls) {
+		super(ls);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Word makeMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

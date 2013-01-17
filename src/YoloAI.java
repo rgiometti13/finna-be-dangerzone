d9efd81475fc547d7c10dc6948d49232be6d7f04
Rob <Rob@Rob-PC.kentdenver.org>
2013-01-17 09:09:51 -0700

    win game method

--- src/YoloAI.java
+++ src/YoloAI.java
@@ -1,18 +1,47 @@
 import core.LetterBag;
 import core.Player;
 import core.Word;
+import core.Board;
+import core.Letter;
+import core.Dictionary;
+import core.Space;
+import core.TestDriver;
 
 public class YoloAI extends Player {
-
+	public Board board;
+	
+	
 	public YoloAI(LetterBag ls) {
 		super(ls);
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	
+	
+	public void readBoard(Board b){
+		board=b;
+	}
+	
 	public Word makeMove() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	
+	//wins the game
+	public void winGame(){
+		for(int x=0;x<15;x++){
+			for(int y=0;y<15;y++){
+				
+			}
+		}
+		
+		
+		
+	}
+	//finds all words off of a given letter on the board
+	public Word[] findWords(Letter a){
+		Dictionary d=new Dictionary();
+		for(int i=2;i<)
+	}
 }
